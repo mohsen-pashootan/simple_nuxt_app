ddd7d0d52ff0eb668249d8320d4c18cf1b14ce7c
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2024-10-15 18:15:22 +0330

    feat(logout)implement logout logic with ui button

--- store/index.js
+++ store/index.js
@@ -126,11 +126,20 @@ const createStore = () => {
           expirationDate = localStorage.getItem("tokenExpiration");
         }
         if (new Date().getTime() > +expirationDate || !token) {
-          console.log('No token or invalid token');
-          vuexContext.commit('clearToken');
+          console.log("No token or invalid token");
+          vuexContext.dispatch("logout");
           return;
         }
         vuexContext.commit("setToken", token);
+      },
+      logout(vuexContext) {
+        vuexContext.commit("clearToken");
+        Cookie.remove("jwt");
+        Cookie.remove("expirationDate");
+        if (process.client) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("tokenExpiration");
+        }
       }
     },
     getters: {
