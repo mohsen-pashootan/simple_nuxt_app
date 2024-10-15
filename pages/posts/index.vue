d43462015f9768dd446890a92d40bf78f7681c17
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2024-10-15 17:35:01 +0330

    feat(auth)get and set token and use it to api call

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -6,67 +6,15 @@
 
 <script>
 export default {
+  middleware: 'log',
   computed: {
     loadedPosts() {
-      return this.$store.getters.loadedPosts;
-    },
-  },
-
-  // #1 we could use fetch instead of asyncData
-  // #2 we could move all logic of asyncData or fetch to store
-  // asyncData(context) {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve({
-  //         loadedPosts: [
-  //           {
-  //             id: "1",
-  //             title: "First Post",
-  //             previewText: "This is our first post!",
-  //             thumbnail:
-  //               "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
-  //           },
-  //           {
-  //             id: "2",
-  //             title: "Second Post",
-  //             previewText: "This is our second post!",
-  //             thumbnail:
-  //               "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
-  //           },
-  //         ],
-  //       });
-  //     }, 1000);
-  //     // reject(new Error())
-  //   })
-  //     .then((data) => {
-  //       return data;
-  //     })
-  //     .catch((e) => {
-  //       context.error(e);
-  //     });
-  //   // calcback
-  //   // callback(null, {
-  //   //     loadedPosts: [
-  //   //       {
-  //   //         id: "1",
-  //   //         title: "First Post",
-  //   //         previewText: "This is our first post!",
-  //   //         thumbnail:
-  //   //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-  //   //       },
-  //   //       {
-  //   //         id: "2",
-  //   //         title: "Second Post",
-  //   //         previewText: "This is our second post!",
-  //   //         thumbnail:
-  //   //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-  //   //       }
-  //   //     ]
-  //   //   });
-  //   // }, 1000);
-  // },
+      return this.$store.getters.loadedPosts
+    }
+  }
 };
 </script>
+
 
 <style scoped>
 .posts-page {
