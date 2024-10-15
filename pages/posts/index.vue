5087df7c6c7ef6bb79110c3bcb7fc0bea11d53a1
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2024-10-15 13:50:09 +0330

    feat(store)add store of vuex to handle data

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,57 +11,65 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: "1",
-              title: "First Post",
-              previewText: "This is our first post!",
-              thumbnail:
-                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
-            },
-            {
-              id: "2",
-              title: "Second Post",
-              previewText: "This is our second post!",
-              thumbnail:
-                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
-            },
-          ],
-        });
-      }, 1000);
-      // reject(new Error())
-    })
-      .then((data) => {
-        return data;
-      })
-      .catch((e) => {
-        context.error(e);
-      });
-    // calcback
-    // callback(null, {
-    //     loadedPosts: [
-    //       {
-    //         id: "1",
-    //         title: "First Post",
-    //         previewText: "This is our first post!",
-    //         thumbnail:
-    //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-    //       },
-    //       {
-    //         id: "2",
-    //         title: "Second Post",
-    //         previewText: "This is our second post!",
-    //         thumbnail:
-    //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-    //       }
-    //     ]
-    //   });
-    // }, 1000);
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    },
   },
+
+  // #1 we could use fetch instead of asyncData
+  // #2 we could move all logic of asyncData or fetch to store
+  // asyncData(context) {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve({
+  //         loadedPosts: [
+  //           {
+  //             id: "1",
+  //             title: "First Post",
+  //             previewText: "This is our first post!",
+  //             thumbnail:
+  //               "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+  //           },
+  //           {
+  //             id: "2",
+  //             title: "Second Post",
+  //             previewText: "This is our second post!",
+  //             thumbnail:
+  //               "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+  //           },
+  //         ],
+  //       });
+  //     }, 1000);
+  //     // reject(new Error())
+  //   })
+  //     .then((data) => {
+  //       return data;
+  //     })
+  //     .catch((e) => {
+  //       context.error(e);
+  //     });
+  //   // calcback
+  //   // callback(null, {
+  //   //     loadedPosts: [
+  //   //       {
+  //   //         id: "1",
+  //   //         title: "First Post",
+  //   //         previewText: "This is our first post!",
+  //   //         thumbnail:
+  //   //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+  //   //       },
+  //   //       {
+  //   //         id: "2",
+  //   //         title: "Second Post",
+  //   //         previewText: "This is our second post!",
+  //   //         thumbnail:
+  //   //           "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+  //   //       }
+  //   //     ]
+  //   //   });
+  //   // }, 1000);
+  // },
 };
 </script>
 
